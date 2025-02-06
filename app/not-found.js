26a8bfa9c93961bd7fe6ebe68208619a25a87d68
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-02-06 20:38:51 +0000

    implementing not found page handler

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+
+import './globals.css'
+
+export default function NotFound(){
+    return <main className="not-found">
+        <h1>Not found!!!</h1>
+        <p> We could not find the rquested page or resource</p>
+    </main>
+}
