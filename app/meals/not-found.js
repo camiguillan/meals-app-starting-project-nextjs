a281a90052292552078717b5a4587a535900c643
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-02-12 18:55:42 +0000

    adding image picker!

--- app/meals/not-found.js
+++ app/meals/not-found.js
@@ -4,6 +4,6 @@ import '../globals.css'
 export default function NotFound(){
     return <main className="not-found">
         <h1>Meal Not found!!!</h1>
-        <p> We could not find the requested page or meal</p>
+        <p> We could not find the requested page or meal data</p>
     </main>
 }
