62aa06de0db2456b9038d8e9545d10eb27a1e01d
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-02-17 18:51:26 +0000

    adding meta data to meals page

--- app/meals/layout.js
+++ app/meals/layout.js
@@ -1,3 +1,10 @@
+export const metadata = {
+    title: 'All Meals',
+    description: 'Browse the delicious shared by our community',
+  };
+  
+
+
 export default function MealsLayout({children}){
     return <>
         {children}
